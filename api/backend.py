88eb92680d42363ebe6f00db87182c5ce2816bba
BG-bibek <bibekgautam810@gmail.com>
2021-08-01 16:33:34 +0545

    V2

--- api/backend.py
+++ api/backend.py
@@ -10,8 +10,8 @@ from apiclient.discovery import build
 app = Flask(__name__,template_folder='template')
 CORS(app)
 tfvect = TfidfVectorizer(stop_words='english', max_df=0.7) #setence laii number convert(max_df ~ more frequency then 0.7 is ignored)
-loaded_model = pickle.load(open('api\model.pkl', 'rb'))  # trained model loaded, pickle- model ko extension(save garxa)
-df = pd.read_csv('api\manual_testing.csv')  #dataset frame
+loaded_model = pickle.load(open('api\modal', 'rb'))  # trained model loaded, pickle- model ko extension(save garxa)
+df = pd.read_csv('api\dataset_manual_testing.csv')  #dataset frame
 x = df['title']  #newss_article
 y = df['label']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state = 0) #training:80 and testing:20 
@@ -44,7 +44,8 @@ def home():
 @app.route('/predict/<string:msg>', methods=['GET'])
 def predict(msg : str):#
     message = msg;  #fetch news article to message.
-    pred = fake_news_det(message)      #predict garera denaxa.
+    # message = "Seven Iranians freed in the prisoner swap have not returned to Iran";
+    pred = rand(message)      #predict garera denaxa.
     print(pred)  #pred = fake or real. 
     verified_sites = verification(message)
     result = {
@@ -57,58 +58,5 @@ def predict(msg : str):#
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-
-
-
-
-#  AIzaSyAY51ACr-o-0hMYZpPKctbKwGonrO80GmU
-
-
-
-#using sys.
-
-# import sys
-
-# #test = sys.argv[1];
-    
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# from sklearn.linear_model import PassiveAggressiveClassifier
-# import pickle #learn about pickle
-# import pandas as pd
-# from sklearn.model_selection import train_test_split
-
-# tfvect = TfidfVectorizer(stop_words='english', max_df=0.7) #setence laii number convert(max_df ~ more frequency then 0.7 is ignored)
-# loaded_model = pickle.load(open('model_test', 'rb'))  # trained model loaded, pickle- model ko extension(save garxa)
-# df = pd.read_csv('manual_testing.csv')  #dataset frame
-# x = df['title']  #newss_article
-# y = df['label']
-# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state = 0) #training:80 and testing:20 
-
-
-
-# def fake_news_det(manual_testing):
-#     tfid_x_train = tfvect.fit_transform(x_train)#english news to number ma convert #kasarii convert gareko xa 
-#     tfid_x_test = tfvect.transform(x_test)      #number ma convert
-#     input_data = [manual_testing] 
-#     vectorized_input_data = tfvect.transform(input_data)     #
-#     prediction = loaded_model.predict(vectorized_input_data) #funciton arg in model
-#     return prediction
-#     #return vectorized_input_data;
-
-# def predict(test):
-#         if len(test) != 0: 
-#             message = test  #fetch news article to message.
-#             pred = fake_news_det(message)      #predict garera denaxa.
-#             print(pred)  #pred = fake or real.
-#         else:
-#             print("please enter something!");
-#        # return render_template('index.html', prediction=pred)  
-#    # else:
-#       #  return render_template('index.html', prediction="Something went wrong")
-# #tests = sys.argv[1];
-# tests = "Lovato Announces 2018 North American Tour Dates With DJ Khaled";
-# print(tests);
-#predict(tests);
 
 
--- popup.js
+++ popup.js
@@ -1,89 +1,51 @@
 let main2 = document.getElementById("main2");
 let result = document.getElementById('result');
 let fake_case = document.getElementById('fake_case');
+let linkList = document.getElementById('linkList');
+let site_array = [];
+let innerLis = "";
 main2.style.display = 'none';
+let innerLi = "";
 document.addEventListener("DOMContentLoaded", () => {
   let wrapper = document.getElementById("wrapper");
   let main = document.getElementById("main");
-
 document.getElementById("sucess").addEventListener("click", async()=> {
   
     let toCheckArticle = document.getElementById("article").value;
     console.log(toCheckArticle);
     let data = [];
     if(toCheckArticle) {
-
-
     fetch(`http://127.0.0.1:5000/predict/${toCheckArticle}`)
     // Handle success
     .then(response => response.json())  // convert to json
     .then(json => {
       data = json.result;
-      console.log(data.predict)
-      
-        main.style.display = 'none';
-        main2.style.display = 'block';
-       
+      console.log(data.predict);
+      console.log(data.verified_sites.items);
+      for(let i = 0; i < 4 ; i++) {
+        console.log(data.verified_sites.items[i].displayLink);
+        console.log(data.verified_sites.items[i].link);
+        site_array.push(data.verified_sites.items[i].link);
+      } 
+        main.style.display = 'none';  
+        main2.style.display = 'block'; 
       result.innerHTML =  `This article is ${data.predict}`;
+      const articleHTML = site_array.map(site => {   
+        innerLi = `<li class = "article-link">
+        <a class="btn" href="${site}" rel="noopener noreforrer"
+        target="_blank">Visit</a>
+        ${site}
+        </li>`;
+        innerLis = innerLis.concat(innerLi); 
+      });
+      console.log(innerLis);
+      linkList.innerHTML = innerLis;
       if(data.predict === 'false') {
         fake_case.style.display = 'none';
       }
     })    //print data to console
     .catch(err => console.log('Request Failed', err)); // Catch errors
-
     }
-    
-   
    console.log(data);
-   
     }) 
 })
-
-//       // Example POST method implementation:
-// async function postData(url = `http://127.0.0.1:5000/predict/hello`, data = {}) {
-//   const response = await fetch(url, {
-//     method: 'GET', 
-//     mode: 'no-cors', 
-//     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-//     credentials: 'same-origin', // include, *same-origin, omit
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     redirect: 'follow', // manual, *follow, error
-//     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-//     body: JSON.stringify(data) // body data type must match "Content-Type" header
-//   });
-//   //return response.json(); // parses JSON response into native JavaScript objects
-// }
-
-// postData(`http://127.0.0.1:5000/predict/hello`, { answer: 42 })
-//   .then(data => {
-//     console.log(data); // JSON data parsed by `data.json()` call
-//   }); 
-//   }
-// }); 
-//  });
-
-// // Future JavaScript will go here;
-    //   fetch("http://127.0.0.1:5000/")
-
-
-
-
-
-
-
-
-
-
-
-    // try to run python in js using sis
-    // const test = require('child_process').spawn;
-    // const process = test('python',['./backend.py','Lovato Announces 2018 North American Tour Dates With DJ Khaled']);
-    // process.stdout.on('data', data => {
-    // console.log(data.toString());
-    // });
-
-
-
-// asdf
